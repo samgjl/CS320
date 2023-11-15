4e83933295b23202801076f58219501c6fa8a645
Sam Johnson-Lacoss <99389146+samgjl@users.noreply.github.com>
2023-11-15 17:00:49 -0600

    yuh

--- model2.py
+++ model2.py
@@ -1,4 +1,3 @@
-# THIS IS OUR FIRST MODEL TEST
 import os
 import numpy as np
 import pathlib
@@ -6,6 +5,7 @@ import IPython.display as display
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import tensorflow as tf
+from tensorflow_examples.models.pix2pix import pix2pix
 # import tensorflow_datasets as tfd
 import keras
 import keras_cv
@@ -14,8 +14,18 @@ from keras.models import Sequential
 from tqdm import tqdm
 import sklearn
 from sklearn.model_selection import train_test_split
-from custom_functions import *
+import importlib.util
+import sys
 from data_reader import DataReader
+
+train_X_masterpath = "data/train/images"
+train_y_masterpath = "data/train/targets"
+# Get data:
+dr = DataReader(train_X_masterpath, train_y_masterpath)
+dr.get_file_lists()
+train, val = dr.get_tf_data(new_size = (256, 256), desired_amount = 1000)
+
+print(type(train), "\n", type(val))
 
 #setting the batch size
 BATCH = 32
@@ -27,17 +37,13 @@ BUFFER = 1000
 STEPS_PER_EPOCH = 800//BATCH
 VALIDATION_STEPS = 200//BATCH
 
-dr = DataReader("data/train/images", "data/train/targets")
-dr.get_file_lists()
-train, val = dr.get_tf_data(new_size=(128, 128))
 train = dr.augment(train)
-
 train = train.cache().shuffle(BUFFER).batch(BATCH).repeat()
 train = train.prefetch(buffer_size=AT)
 val = val.batch(BATCH)
 
 # Use pre-trained DenseNet121 without head
-base = keras.applications.DenseNet121(input_shape=[128,128,3],
+base = keras.applications.DenseNet121(input_shape=[256,256,3],
                                       include_top=False,
                                       weights='imagenet')
 
@@ -55,10 +61,7 @@ skip_outputs = [base.get_layer(name).output for name in skip_names]
 downstack = keras.Model(inputs=base.input,
                        outputs=skip_outputs)
 # freeze the downstack layers
-downstack.trainable = False
-
-
-from tensorflow_examples.models.pix2pix import pix2pix
+downstack.trainable = True ########################################################## ORIGINALLY SET TO FALSE
 
 # Four upstack layers for upsampling sizes
 # 4->8, 8->16, 16->32, 32->64
@@ -68,7 +71,7 @@ upstack = [pix2pix.upsample(512,3),
           pix2pix.upsample(64,3)]
 
 # define the input layer
-inputs = keras.layers.Input(shape=[128,128,3])
+inputs = keras.layers.Input(shape=[256,256,3])
 
 # downsample
 down = downstack(inputs)
@@ -94,9 +97,8 @@ unet = keras.Model(inputs=inputs, outputs=out)
 
 # compiling the model
 def Compile_Model():
-    unet.compile(loss=keras.losses.BinaryCrossentropy(from_logits=True),
-            # optimizer=keras.optimizers.RMSprop(learning_rate=0.001), # THIS IS THE ORIGINAL!!
-            optimizer=keras.optimizers.RMSprop(learning_rate=0.0005),
+    unet.compile(loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            optimizer=keras.optimizers.RMSprop(learning_rate=0.001),
             metrics=['accuracy'])
 Compile_Model()
 
@@ -105,7 +107,7 @@ hist_1 = unet.fit(train,
                validation_data=val,
                steps_per_epoch=STEPS_PER_EPOCH,
                validation_steps=VALIDATION_STEPS,
-               epochs=50,
+               epochs=20,
                verbose=1)
 
 # select a validation data batch
@@ -140,5 +142,5 @@ for i in pred:
     k += 1
     if k == 4: break
 plt.suptitle('Prediction After 20 Epochs (No Fine-tuning)', color='red', size=20)
+plt.savefig("result.png")
 plt.show()
-plt.savefig("model1_results/results.png")
